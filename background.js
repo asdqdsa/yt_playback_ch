9ff46223ee631b7decc75f1edc0b82f381a32c42
asdqdsa <asdqdsa@protonmail.com>
2022-08-06 09:38:15 +0400

    Being faster than light brings only darkness

--- background.js
+++ background.js
@@ -1,7 +1,44 @@
 /* let color = '#3aa757';
+let speed;
 
 chrome.runtime.onInstalled.addListener(() => {
 	chrome.storage.sync.set({ color });
 	console.log('Default background color set to %cblack', `color: ${color}`);
-});
+}); */
+
+/* window.postMessage({
+	type: "FROM_PAGE_TO_POPUP",
+	text: document.getElementsByTagName("video")[0].playbackRate,
+}, "*"); */
+
+/* chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting === "hello")
+      sendResponse({farewell: "goodbye"});
+  }
+) */
+
+/* let currentSpeed = document.getElementById("currentSpeed"); */
+
+/* chrome.storage.sync.set("speed", ({speed}) => {
+	currentSpeed.textContent = "???";
+}); */
+
+/* chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		console.log(request, sender, sendResponse('111'), '!!');
+	}); */
+
+/* 	chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+		const {speed} = request;
+		chrome.storage.sync.set(request);
+
+	}); */
+
+/* 	window.addEventListener("message", function(event) {
+		document.getElementById("currentSpeed").textContent = event.data.type;
+	});
  */
--- popup.js
+++ popup.js
@@ -1,11 +1,18 @@
 let speedUp = document.getElementById("speedUp");
-let speedDown = document.getElementById("speedDown");
+let speedDown = document.getElementById("slowDown");
 let currentSpeed = document.getElementById("currentSpeed");
 
-chrome.storage.sync.get("color", ({ color }) => {
+/* chrome.storage.sync.get("color", ({ color }) => {
 	speedUp.style.backgroundColor = color;
-});
+}); */
 
+
+
+/* chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+    console.log(response.farewell);
+  });
+}); */
 
 // When the button is clicked, inject increasePlayBackSpeed into current page
 speedUp.addEventListener("click", async () => {
@@ -36,19 +43,25 @@ speedDown.addEventListener("click", async () => {
 
 // The body of this function will be executed as a content script inside the
 // current page
+/* x */
 function increasePlayBackSpeed() {
   chrome.storage.sync.get("color", ({ color }) => {
-
-		document.getElementsByTagName("video")[0].playbackRate += 0.5;
+    document.getElementsByTagName("video")[0].playbackRate += 0.5;
 		console.log(document.getElementsByTagName("video")[0].playbackRate);
-		currentSpeed.textContent = document.getElementsByTagName("video")[0].playbackRate;
   });
+/*   chrome.runtime.sendMessage({mes: document.getElementsByTagName("video")[0].playbackRate.toString()}); */
+
 }
+
 function decreasePlayBackSpeed() {
   chrome.storage.sync.get("color", ({ color }) => {
 
-		document.getElementsByTagName("video")[0].playbackRate -= 0.5;
+    document.getElementsByTagName("video")[0].playbackRate -= 0.5;
 		console.log(document.getElementsByTagName("video")[0].playbackRate);
-
   });
 }
+
+chrome.scripting.executeScript({
+  target: { tabId: tab.id },
+  function: ()=> window.addEventListener('keydown', e => console.log(e)),
+});
